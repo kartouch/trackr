9945c07259ba67e77294f7b219398422be88528f
Christophe Augello <christophe@augello.be>
2018-06-28 14:26:18 +0200

    paginate fix

--- static/app.js
+++ static/app.js
@@ -1,34 +1,36 @@
 'use strict';
 
 var app = angular.module('trackr', []);
-app.controller('trackr', function($scope,$http) {
+app.controller('trackr', function ($scope, $http) {
 
     $http.get("/api/v1/tracks")
-        .then(function(response) {
-        $scope.tracks = response.data;
-        $scope.tracksPaginated = $scope.tracks.slice(0,10)
-        var p = Math.ceil($scope.tracks.length/10)
-        $scope.pages = new Array(p)
-    }); 
+        .then(function (response) {
+            $scope.tracks = response.data;
+            $scope.tracksPaginated = $scope.tracks.slice(0, 10)
+            var p = Math.ceil($scope.tracks.length / 10)
+            $scope.pages = new Array(p)
+        });
 
     $scope.markAsChecked = track => {
-        $http.post("/api/v1/tracks/"+track.id+"/checked")
-        .then(
-            function(response){
-              $('#track_'+track.id).hide()
-            }
-         );
-      };
+        $http.post("/api/v1/tracks/" + track.id + "/checked")
+            .then(
+                function (response) {
+                    $('#track_' + track.id).hide()
+                }
+            );
+    };
 
 
-    $scope.paginateTracks = (tracks,page) =>{
-        if (tracks == undefined){ return }
-        var p = Math.ceil(tracks.length/10)
+    $scope.paginateTracks = (tracks, page) => {
+        if (tracks == undefined) {
+            return
+        }
+        var p = Math.ceil(tracks.length / 25)
         $scope.pages = new Array(p)
-        var start = (page * 10) - 10
+        var start = (page * 25) - 25
         var end = start + 9
-        var trackList = tracks.slice(start,end)
+        var trackList = tracks.slice(start, end)
         $scope.tracksPaginated = trackList
     };
-    
+
 });
