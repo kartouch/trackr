f02f29b6d6067a8f72bf0996b2d485c77daea6f1
Christophe Augello <caugello>
2018-07-05 09:16:21 +0200

    sqlite tst

--- wsgi.py
+++ wsgi.py
@@ -15,23 +15,19 @@ application.config['SECRET_KEY'] = 'secret!102ine10i2en19919199129'
 
 db_name = os.environ['TRACKR_DATA'] + '/trackr.db'
 timer = 120
-conn = sqlite3.connect(db_name, timeout=10)
+conn = sqlite3.connect(db_name, timeout=10, isolation_level=None)
 c = conn.cursor()
 c.execute(
     """CREATE TABLE IF NOT EXISTS tracks(id INTEGER PRIMARY KEY AUTOINCREMENT,
        artist text, title text,checked boolean,
        CONSTRAINT track_unique UNIQUE (artist, title));""")
-conn.close()
 
 
 def insert(artist, title):
     current_tracks = all_tracks()
-    conn = sqlite3.connect(db_name, timeout=10)
-    c = conn.cursor()
     c.execute('INSERT INTO tracks (artist,title,checked) VALUES (?,?,?)',
               (artist, title, False))
     conn.commit()
-    conn.close()
     actual_tracks = all_tracks()
     if len(current_tracks) != len(actual_tracks):
         print "Added %s - %s" % (artist, title)
@@ -39,18 +35,14 @@ def insert(artist, title):
 
 def mark_as_checked(idx):
     conn = sqlite3.connect(db_name, timeout=10)
-    c = conn.cursor()
     c.execute('UPDATE tracks SET checked=? WHERE id=?', (1, idx))
     conn.commit()
-    conn.close()
 
 
 def all_tracks():
     conn = sqlite3.connect(db_name, timeout=10)
-    c = conn.cursor()
     c.execute('SELECT * from tracks where checked = 0;')
     tracks = c.fetchall()
-    conn.close()
     track_list = []
     for track in tracks:
         at = {
